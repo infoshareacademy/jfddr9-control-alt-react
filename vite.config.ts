1f9b75cbee5fcf7997db9c56bb7e768988e03aaa
ToJurek <tomaszjurkiewicz36@gmail.com>
2023-05-09 10:36:21 +0200

    allowJS

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,21 @@ export default defineConfig({
   build: {
     outDir: "./build",
   },
+  esbuild: {
+    loader: "tsx", // OR "jsx"
+    include: [
+      // Add this for business-as-usual behaviour for .jsx and .tsx files
+      "src/**/*.jsx",
+      "src/**/*.tsx",
+      "node_modules/**/*.jsx",
+      "node_modules/**/*.tsx",
+      // Add these lines to allow all .js files to contain JSX
+      "src/**/*.js",
+      "node_modules/**/*.js",
+
+      // Add these lines to allow all .ts files to contain JSX
+      "src/**/*.ts",
+      "node_modules/**/*.ts",
+    ],
+  },
 });
