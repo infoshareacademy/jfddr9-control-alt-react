416d384d415e5e9a06a8317ed0f2cb67dc5b97a8
ToJurek <tomaszjurkiewicz36@gmail.com>
2023-05-10 13:22:53 +0200

    change BrowerRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import App from "./App.js";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
