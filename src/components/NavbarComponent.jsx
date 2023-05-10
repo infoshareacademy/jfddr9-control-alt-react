c580acd8c741f78705c6bd7fe7d970e323b34b9a
ToJurek <tomaszjurkiewicz36@gmail.com>
2023-05-10 14:52:33 +0200

    change project to jsx

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
-export const NavbarComponent = (props: any) => {
+export const NavbarComponent = (props) => {
   return (
     <>
       <Navbar bg="dark" variant="dark">
