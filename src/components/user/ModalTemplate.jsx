2346dd15f15afb5112afee0891eedbbe9ff80613
JRW <jakub.robert.w@gmail.com>
2023-05-11 14:02:22 +0200

    addded some style to modals and buttons

--- src/components/user/ModalTemplate.jsx
+++ src/components/user/ModalTemplate.jsx
@@ -3,8 +3,14 @@ import Modal from "react-bootstrap/Modal";
 export const ModalTemplate = (props) => {
   return (
     <Modal show={props.showR} onHide={props.handleCloseR}>
-      <Modal.Header closeButton>
-        <Modal.Title>{props.title}</Modal.Title>
+      <Modal.Header>
+        <Modal.Title>
+          <div className="logo modal-logo">
+            <b>
+              <span>{props.title}</span>
+            </b>
+          </div>
+        </Modal.Title>
       </Modal.Header>
       <Modal.Body>{props.child}</Modal.Body>
     </Modal>
