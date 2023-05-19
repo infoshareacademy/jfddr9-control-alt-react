229d8cd6a9af67507b9b525673d37e671d18c1b0
Jakub Łuczkiewicz <jakub.luczkiewicz@gmail.com>
2023-05-19 14:27:41 +0200

    Added ingredient listing.

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -1,12 +1,48 @@
-export const IngredientList = ({ list }) => {
+import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+const Listitem = ({ ingredient, count, glassPour }) => {
+  const [visible, setVisible] = useState("");
+
+  const show = async (ms) => {
+    await new Promise((pr) => setTimeout(pr, ms));
+    setVisible("show");
+    glassPour();
+  };
+  useEffect(() => {
+    show(1000 * count);
+  }, []);
+  console.log(ingredient);
+
+  return <li className={`ingredient ${visible}`}>{ingredient}</li>;
+};
+export const IngredientList = ({ list, glassPour }) => {
+  let count = 1;
+  const [showList, setShowList] = useState(false);
   return (
     <div>
       <h4>Ingredients</h4>
-      <ul className="ingredient-list">
-        {list.map((ingredient) => {
-          return <li key={ingredient}>{ingredient}</li>;
-        })}
-      </ul>
+      <Button
+        className="general-btn green-hover"
+        onClick={() => setShowList(true)}
+      >
+        Mix
+      </Button>
+      {showList && (
+        <ul className="ingredients">
+          {list.map((ingredient) => {
+            // glassPour();
+            count = count + 1;
+            return (
+              <Listitem
+                key={ingredient + count}
+                ingredient={ingredient}
+                count={count}
+                glassPour={glassPour}
+              />
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
--- src/components/pages/MixIt.jsx
+++ src/components/pages/MixIt.jsx
@@ -21,7 +21,7 @@ export const MixIt = () => {
   const [previousViewName, setPreviousViewName] = useState("");
   const [favoriteDrinkNames, setFavoriteDrinkNames] = useState([]);
   const [isFavorite, setIsFavorite] = useState(false);
-
+  const [height, setWaterHeight] = useState(160);
   function changeView(newViewName) {
     setPreviousViewName(viewName);
     setViewName(newViewName);
@@ -40,13 +40,17 @@ export const MixIt = () => {
     if (shown !== null) shown.classList.add("show");
   }, [viewName]);
 
-  const [height, setWaterHeight] = useState(160);
-
   function glassPour() {
     const ingredientsCount = selectedOption.ingredients.length;
     let waterHeightStep = 220 / ingredientsCount;
     setWaterHeight(height - waterHeightStep);
+    console.log("Glass Pour Fn");
+    console.log(height);
   }
+  useEffect(() => {
+    console.log("Main");
+    console.log(height);
+  }, [height]);
   document.documentElement.style.setProperty("--liquid-height", `${height}px`);
 
   return (
@@ -135,7 +139,10 @@ export const MixIt = () => {
         child={
           <>
             {selectedOption !== null ? (
-              <IngredientList list={selectedOption.ingredients} />
+              <IngredientList
+                list={selectedOption.ingredients}
+                glassPour={glassPour}
+              />
             ) : (
               <div></div>
             )}
